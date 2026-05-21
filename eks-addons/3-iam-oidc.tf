74694c94948006895a7fbb25d1e746b8660e9e44
Amit Kumar <amit.active2008@gmail.com>
2026-05-21 12:45:55 +0530

    added extra support

--- eks-addons/3-iam-oidc.tf
+++ eks-addons/3-iam-oidc.tf
@@ -1,3 +1,23 @@
+# Policy to get s3 read access to the pods in default NS having service account name aws-s3-access
+
+data "aws_iam_policy_document" "oidc_assume_role_policy" {
+  statement {
+    actions = ["sts:AssumeRoleWithWebIdentity"]
+    effect  = "Allow"
+
+    condition {
+      test     = "StringEquals"
+      variable = "${replace(data.aws_iam_openid_connect_provider.this.url, "https://", "")}:sub"
+      values   = ["system:serviceaccount:default:aws-s3-access"]
+    }
+
+    principals {
+      identifiers = [data.aws_iam_openid_connect_provider.this.arn]
+      type        = "Federated"
+    }
+  }
+}
+
 resource "aws_iam_role" "test_oidc" {
   count = var.enable_eks_oidc ? 1 : 0
   assume_role_policy = data.aws_iam_policy_document.oidc_assume_role_policy.json
